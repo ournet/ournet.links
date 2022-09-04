cdc24bebf9d09fa5aa8426ae9dbb0068bfbbfb29
Dumitru C <canteadumitru@gmail.com>
2022-09-04 22:37:06 +0300

    vrijeme.one

--- src/cdn.ts
+++ src/cdn.ts
@@ -2,15 +2,22 @@ const WI_HOST = "//wi.ournetcdn.net";
 const ASSETS_HOST = "//assets.ournetcdn.net";
 
 export const wi = {
-  stories: function(size: string, id: string): string {
+  stories: function (size: string, id: string): string {
     return (
-      WI_HOST + "/stories/" + id.substr(0, 4) + "/" + size + "/" + id + ".jpg"
+      WI_HOST +
+      "/stories/" +
+      id.substring(0, 4) +
+      "/" +
+      size +
+      "/" +
+      id +
+      ".jpg"
     );
   },
 
-  news: function(size: string, id: string): string {
+  news: function (size: string, id: string): string {
     return (
-      WI_HOST + "/news/" + id.substr(0, 4) + "/" + size + "/" + id + ".jpg"
+      WI_HOST + "/news/" + id.substring(0, 4) + "/" + size + "/" + id + ".jpg"
     );
   }
 };
@@ -27,7 +34,7 @@ const COUNTRY_MAP: { [index: string]: string } = {
 
 export const assets = {
   img: {
-    logo: function(nameOrCountry: string): string {
+    logo: function (nameOrCountry: string): string {
       if (nameOrCountry && nameOrCountry.length === 2) {
         nameOrCountry = COUNTRY_MAP[nameOrCountry] || "ournet";
       }
